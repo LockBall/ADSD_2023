26569912ba7a54e4e231d91d213ce2d94faf8102
LockBall <johnplutz@gmail.com>
2023-10-20 18:48:39 -0400

    changed ram to be std_logic and altered adressing into it

--- projects/1_proj/2_part/top_level/RAM.vhd
+++ projects/1_proj/2_part/top_level/RAM.vhd
@@ -6,21 +6,21 @@ use ieee.numeric_std.all;
 
 entity RAM is
     Port (
-        data_in   : in  std_logic_vector(7 downto 0);
-        address   : in  std_logic_vector(7 downto 0);
+        data_in   : in  std_logic; --_vector(7 downto 0);
+        address   : in  natural;
         we        : in  std_logic;
-        data_out  : out std_logic_vector(7 downto 0)
+        data_out  : out std_logic --_vector(7 downto 0)
     );
 end RAM;
 
 architecture Behavioral of RAM is
-    type ram_type is array (0 to 255) of std_logic_vector(7 downto 0);
-    signal ram : ram_type := (others => "00000000");
+    type ram_type is array (0 to 255) of std_logic; --_vector(7 downto 0);
+    signal ram : ram_type ; --:= '0'; --(others => "00000000");
 begin
     process (address, data_in, we)
-        variable addr : integer;
+        variable addr : natural;
     begin
-        addr := to_integer(unsigned(address));
+        addr := address;
 
         if we = '1' then
             ram(addr) <= data_in;
--- projects/1_proj/2_part/top_level/top_level.vhd
+++ projects/1_proj/2_part/top_level/top_level.vhd
@@ -8,6 +8,7 @@ entity top_level is
     generic ( --needs to be first
         constant ro_count : positive range 3 to 37 := 15 --RO use ODD to get even # ring osc
     );
+    
     port (
         clock        : in std_logic; --P11 50 MHZ
         reset_btn    : in std_logic;
@@ -17,6 +18,7 @@ entity top_level is
         done_LED     : out std_logic := '0';
         response     : buffer std_logic
     );
+    
 end entity top_level;
 
 architecture RTL of top_level is
@@ -33,21 +35,22 @@ architecture RTL of top_level is
     signal chal_rit       : std_logic_vector(0 to 5) := "000001";
 
     -- RAM access variables
-	 type ram_type is array (0 to 511) of std_logic; 
-    signal challenge       : std_logic_vector(0 to 5);
+	type ram_type is array (0 to 511) of std_logic; 
+    
+    --signal challenge       : std_logic_vector(0 to 5);
     signal reset_active    : std_logic := '0';
     signal enable_active   : std_logic := '0';
     signal probe_delay     : time := 0.0 ms;  -- probe delay set to 0 for now
-	 signal read_data       : std_logic_vector(7 downto 0); -- read_data function for output of RAM data
+	signal read_data       : std_logic_vector(7 downto 0); -- read_data function for output of RAM data
     signal store_result    : std_logic;
 	 
-	 signal ram_data_in     : std_logic_vector(7 downto 0);
-	 signal ram_address     : std_logic_vector(7 downto 0);
-	 signal ram_we          : std_logic;
-	 signal ram_data_out    : std_logic_vector(7 downto 0);
+	signal ram_data_in     : std_logic; --_vector(7 downto 0);
+	signal ram_address     : natural; --std_logic_vector(7 downto 0);
+	signal ram_we          : std_logic;
+	signal ram_data_out    : std_logic; --_vector(7 downto 0);
 
 
-begin
+  begin
 
 
     ro_puf : entity work.ro_puf port map ( --ro_puf 00 -> top level
@@ -62,7 +65,7 @@ begin
     );
 	 
 	 
-	 RAM_inst : entity work.RAM port map (
+	RAM_inst : entity work.RAM port map (
 		data_in     => ram_data_in,
 		address     => ram_address,
 		we          => ram_we,
@@ -80,6 +83,7 @@ begin
                 reset <= '0';
             end if;
 
+            
             if (done = '0') AND (initial_reset = '1') then
 				
                 case tl_count is
@@ -89,42 +93,37 @@ begin
                         enable_active <= '0'; -- enable => '0' active signal 
                         pulse <= '0'; -- pulse deasserted
                         store_result <= '0';  -- store_result flag cleared
-                        challenge <= chal_lft; -- challenge => ro_puf
+                        --challenge <= chal_lft; -- challenge => ro_puf
                         tl_enable <= '0'; -- signal disabled
 
                 
-					 when 3 =>
+					when 3 =>
                         reset_active <= '0';  -- T3: Deassert reset
                         enable_active <= '0'; -- T4: Assert enable i.e active signal 
                         tl_enable <= '1';
                         pulse <= '1';
 
                   
-						when 8 =>
+                    when 8 =>
                         store_result <= '1';  -- T5: Set store_result flag
                         req_resp_out <= '1';  -- T2: Provide challenge
 
                   
-						when 10 =>
+					when 10 =>
                         resp_ary(resp_cnt) <= response; -- Store the result
                         resp_cnt <= resp_cnt + 1;
                         req_resp_out <= '0';  -- T6: Deassert req_resp_out
 
                    
-						 when 15 =>
+					when 15 =>
                         if to_integer(unsigned(chal_rit)) = ro_count - 1 then
-								
-                            done <= '1';
-									 
+						    done <= '1';
                         elsif to_integer(unsigned(chal_rit)) = ro_count then
-								
                             chal_lft <= chal_lft + 1;
                             chal_rit <= chal_lft + 2;
-                     
-							else
+						else
                             chal_rit <= chal_rit + 1;
-                    
-						  end if;
+						 end if;
 
                     when others =>
                 end case;
@@ -136,35 +135,33 @@ begin
 						  
                 else
                     tl_count <= tl_count + 1;
-						  
-              
-				  end if;
-            end if;
+				end if;
+                
+            end if; -- done, initial_reset
 
+            
             if done = '1' then
 				
                 done_LED <= '1';
 					 
             end if;
 
-           if store_result = '1' then
-
-    ram_data_in <= resp_ary(resp_cnt); --write data => RAM
-    ram_address <= challenge;
-    ram_we <= '1'; 
-end if; 
+            
+			if store_result = '1' then
+				 ram_data_in <= response; -- resp_ary(resp_cnt); --write data => RAM
+				 ram_address <= resp_cnt;
+				 ram_we <= '1'; 
+			end if; 
     
-    if ram_we = '0' then
-	 
-        ram_address <= challenge;  -- Provide the address for reading
-        ram_data_in <= (others => '0');  -- input cleared
-        ram_we <= '0';  -- read function enabled
-        
-        wait for probe_delay;
-        
-        -- Data read from RAM will be available in ram_data_out
-        read_data <= ram_data_out;  
-    end if;
-end if;
-    end process;
-end architecture RTL;
+    
+            if ram_we = '0' then
+                ram_address <= resp_cnt;  -- Provide the address for reading
+                ram_data_in <= '0'; -- (others => '0');  -- input cleared
+                ram_we <= '0';  -- read function enabled
+                --wait for probe_delay;
+                --read_data <= ram_data_out; -- Data read from RAM will be available in ram_data_out
+            end if;
+            
+        end if; -- rising_edge(clock)
+    end process; -- clock
+end architecture RTL; -- of top_level
