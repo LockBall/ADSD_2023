3a52612ac0f499e439aa921cfe7bdebfccbb3eab
LockBall <johnplutz@gmail.com>
2023-12-09 17:58:30 -0500

    intentional write to specific address

--- tests/TDPDC_RAM/TDPDC_RAM.vhd
+++ tests/TDPDC_RAM/TDPDC_RAM.vhd
@@ -1,32 +1,35 @@
 -- Quartus Prime VHDL Template
 -- True Dual-Port RAM with dual clock
---
 -- Read-during-write on port A or B returns newly written data
--- 
 -- Read-during-write on port A and B returns unknown data.
 
 library ieee;
 use ieee.std_logic_1164.all;
 
-
+use ieee.numeric_std.all; 
+ 
 entity TDPDC_RAM is
 
     generic(
-        DATA_WIDTH : natural := 8;
-        ADDR_WIDTH : natural := 6
+        DATA_WIDTH : natural := 16;
+        ADDR_WIDTH : natural := 4
     );
 
     port(
         clk_a   : in std_logic;
         clk_b   : in std_logic;
-        addr_a  : in natural range 0 to 2**ADDR_WIDTH - 1;
-        addr_b  : in natural range 0 to 2**ADDR_WIDTH - 1;
-        data_a  : in std_logic_vector((DATA_WIDTH-1) downto 0);
-        data_b  : in std_logic_vector((DATA_WIDTH-1) downto 0);
-        we_a    : in std_logic := '1';
-        we_b    : in std_logic := '1';
-        q_a     : out std_logic_vector((DATA_WIDTH -1) downto 0);
-        q_b     : out std_logic_vector((DATA_WIDTH -1) downto 0)
+        
+        addr_a  : in std_logic_vector((ADDR_WIDTH - 1) downto 0);  -- natural range 0 to 2**ADDR_WIDTH - 1 := 0 ;
+        addr_b  : in std_logic_vector((ADDR_WIDTH - 1) downto 0);  -- natural range 0 to 2**ADDR_WIDTH - 1;
+        
+        data_a  : in std_logic_vector((DATA_WIDTH - 1) downto 0);
+        data_b  : in std_logic_vector((DATA_WIDTH - 1) downto 0);
+        
+        we_a    : in std_logic := '0';
+        we_b    : in std_logic := '0';
+        
+        q_a     : out std_logic_vector((DATA_WIDTH - 1) downto 0);
+        q_b     : out std_logic_vector((DATA_WIDTH - 1) downto 0)
     );
 
 end TDPDC_RAM;
@@ -40,17 +43,26 @@ architecture rtl of TDPDC_RAM is
 
     -- Declare the RAM 
     shared variable ram : memory_t;
+    
+    signal addr_a_nat : natural range 0 to 2**ADDR_WIDTH - 1 ;
+    signal addr_b_nat : natural range 0 to 2**ADDR_WIDTH - 1 ;
 
   begin
 
     -- Port A
     process(clk_a) 
       begin
-        if(rising_edge(clk_a)) then 
+
+        if(rising_edge(clk_a)) then
+        
+            addr_a_nat <= to_integer(unsigned(addr_a));
+            
             if(we_a = '1') then
-                ram(addr_a) := data_a;
+                ram(addr_a_nat) := data_a;
+                
             end if;
-            q_a <= ram(addr_a);
+            
+            q_a <= ram(addr_a_nat);
         end if;
     end process;
 
@@ -58,11 +70,15 @@ architecture rtl of TDPDC_RAM is
     -- Port B
     process(clk_b)
       begin
-        if(rising_edge(clk_b)) then 
+
+        if(rising_edge(clk_b)) then
+            addr_b_nat <= to_integer(unsigned(addr_b));
+
             if(we_b = '1') then
-                ram(addr_b) := data_b;
+                ram(addr_b_nat) := data_b;
             end if;
-            q_b <= ram(addr_b);
+            
+            q_b <= ram(addr_b_nat);
         end if;
     end process;
 
--- tests/TDPDC_RAM/tb_TDPDC_RAM.vhd
+++ tests/TDPDC_RAM/tb_TDPDC_RAM.vhd
@@ -1,6 +1,9 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+use ieee.std_logic_unsigned.ALL;
+
+
 entity tb_TDPDC_RAM is
 end tb_TDPDC_RAM;
 
@@ -9,67 +12,97 @@ architecture tb of tb_TDPDC_RAM is
     component TDPDC_RAM
 
         generic(
-            DATA_WIDTH : natural := 8;
-            ADDR_WIDTH : natural := 6
+            ADDR_WIDTH : natural := 4 ;
+            DATA_WIDTH : natural := 16 -- x'FA57'
         );
         
         port(
-            clk_a   : in std_logic;
-            clk_b   : in std_logic;
-            addr_a  : in natural range 0 to 2**ADDR_WIDTH - 1;
-            addr_b  : in natural range 0 to 2**ADDR_WIDTH - 1;
-            data_a  : in std_logic_vector((DATA_WIDTH-1) downto 0);
-            data_b  : in std_logic_vector((DATA_WIDTH-1) downto 0);
-            we_a    : in std_logic := '1';
-            we_b    : in std_logic := '1';
-            q_a     : out std_logic_vector((DATA_WIDTH -1) downto 0);
-            q_b     : out std_logic_vector((DATA_WIDTH -1) downto 0)
+            clk_a   :   in std_logic ;
+            clk_b   :   in std_logic ;
+            addr_a  :   in std_logic_vector((ADDR_WIDTH - 1) downto 0);  -- natural range 0 to 2**ADDR_WIDTH - 1;  -- 2^ADDR_WIDTH
+            addr_b  :   in std_logic_vector((ADDR_WIDTH - 1) downto 0);  -- natural range 0 to 2**ADDR_WIDTH - 1;
+            data_a  :   in std_logic_vector((DATA_WIDTH - 1) downto 0);
+            data_b  :   in std_logic_vector((DATA_WIDTH - 1) downto 0);
+            we_a    :   in std_logic ;
+            we_b    :   in std_logic ;
+            q_a     :   out std_logic_vector((DATA_WIDTH - 1) downto 0);
+            q_b     :   out std_logic_vector((DATA_WIDTH - 1) downto 0)
         );
     
     end component TDPDC_RAM;
     
     -- constants
-    constant DATA_WIDTH : integer   := 8;
-    constant ADDR_WIDTH : integer   := 6;
+    constant ADDR_WIDTH : integer   :=  4 ;
+    constant DATA_WIDTH : integer   := 16 ;
     
     -- signals
+    signal clock_1      : std_logic := '0' ;
     signal clock_50     : std_logic := '0' ;
-    
-    
+
     -- inputs
     signal clk_a    : std_logic;
     signal clk_b    : std_logic;
-    signal addr_a   : natural range 0 to 2**ADDR_WIDTH - 1;
-    signal addr_b   : natural range 0 to 2**ADDR_WIDTH - 1;
-    signal data_a   : std_logic_vector((DATA_WIDTH-1) downto 0);
-    signal data_b   : std_logic_vector((DATA_WIDTH-1) downto 0);
-    signal we_a     : std_logic := '1';
-    signal we_b     : std_logic := '1';
+    signal addr_a   : std_logic_vector((ADDR_WIDTH - 1) downto 0) ;  -- natural range 0 to 2**ADDR_WIDTH - 1 := 0 ;
+    signal addr_b   : std_logic_vector((ADDR_WIDTH - 1) downto 0) := "0000" ;  -- natural range 0 to 2**ADDR_WIDTH - 1;
+    signal data_a   : std_logic_vector((DATA_WIDTH - 1) downto 0) ;
+    signal data_b   : std_logic_vector((DATA_WIDTH - 1) downto 0) ;
+    signal we_a     : std_logic := '0';
+    signal we_b     : std_logic := '0';
     
     -- outputs
-    signal q_a      : std_logic_vector((DATA_WIDTH -1) downto 0);
-    signal q_b      : std_logic_vector((DATA_WIDTH -1) downto 0);
+    signal q_a      : std_logic_vector((DATA_WIDTH - 1) downto 0);
+    signal q_b      : std_logic_vector((DATA_WIDTH - 1) downto 0);
 
     
   begin
   
-    clk_a <= clock_50 ;
-    clk_b <= clock_50 ;
+    clock_1  <= not clock_1  after 20 ns;  -- 500, 1 MHz
+    clock_50 <= not clock_50 after 10 ns;  -- 10, 50 MHz
     
-    clock_50 <= not clock_50 after 10 ns;
-  
+    clk_a <= clock_1 ;
+    clk_b <= clock_50 ;
+
+    
+    addr_a  <=  "0001" ;
+    data_a  <= x"5100" ;    
+    we_a    <= '1' after 55 ns , '0' after 65 ns ;
+
+
+    -- clock process
+--    clock_process : process
+--      begin
+--        wait for 10 ns;
+--        clock_50 <= not clock_50;
+--    end process;
+    
+    
+    -- stimulus process
+--    stim_proc : process(clock_50) is
+--    
+--      begin
+--        
+--        if rising_edge(clock_50) then
+--            addr_a <= addr_a + 1 ;
+--            
+--        end if;
+--
+--    end process stim_proc;
     
   
     UUT : TDPDC_RAM  -- entity work.
         port map(
             clk_a   => clk_a,
             clk_b   => clk_b,
+            
             addr_a  => addr_a,
             addr_b  => addr_b,
+            
             data_a  => data_a,
             data_b  => data_b,
+            
             we_a    => we_a,
             we_b    => we_b,
+            
             q_a     => q_a,
             q_b     => q_b
         );
