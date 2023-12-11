bd3acfca0fca8d9c7e31b7256068815ebf006f7a
LockBall <johnplutz@gmail.com>
2023-12-11 11:33:30 -0500

    moved max10_adc to 4_proj

--- tests/VGA_86/VGA_86_tb.vhd
+++ tests/VGA_86/VGA_86_tb.vhd
@@ -13,12 +13,35 @@ architecture sim of VGA_86_TB is
     signal vga_vs_tb : std_logic;                     -- video out (4 bit resolution)
     signal vga_hs_tb : std_logic;
     signal vga_r_tb, vga_g_tb, vga_b_tb : std_logic_vector(0 to 3);
+    
+    
+    signal reset : std_logic;
+    signal clock_40 : std_logic;
+--    signal clock_40_out : std_logic;
+    
+        
+--    component clock_40
+--        PORT(
+--            areset  : in std_logic;
+--            inclk0	: IN STD_LOGIC  := '0';
+--            c0		: OUT STD_LOGIC;
+--            locked  : out std_logic
+--        );
+--    end component;
+        
         
   begin
   
     max10_clk1_50_tb <= not max10_clk1_50_tb after 10 ns;  -- 50 MHz
     key_tb <= "1", "0" after 5000 ns, "1" after 10000 ns ; -- low resets counter, high allows counts
 
+    
+--    clock_40_inst : clock_40 PORT MAP (
+--		areset	 => reset,
+--		inclk0	 => max10_clk1_50_tb,
+--		c0	     => clock_40_out,
+--		locked	 => open -- locked_sig
+--	);
     
     -- connect main code to testbench
     UUT : entity work.VGA_86 port map(
